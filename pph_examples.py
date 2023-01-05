1e2da9c9a021305f52047fa9b431d63c80e6aa70
Chiara Martyka <chiara.martyka@gmail.com>
2023-01-05 17:59:19 +0100

    added second version of interwoven grid to example file

--- pph_examples.py
+++ pph_examples.py
@@ -29,7 +29,7 @@ def torus_knot(p: int, q: int, dim: int, res = 20):
 
 
 
-def interwoven_grid(num_steps=20):
+def interwoven_grid_211(num_steps=20):
     """
     interwoven 3D grid with index 2*1*1
     """
@@ -41,7 +41,7 @@ def interwoven_grid(num_steps=20):
 
     x_direction_second_part_x_vals = np.linspace(0.3, 1.2, num_steps, endpoint=False) 
     x_direction_second_part_y_vals = np.linspace(0.4, 0.2, num_steps, endpoint=False)
-    x_direction_second_part_z_vals = np.linspace(0,     0.4, num_steps, endpoint=False)
+    x_direction_second_part_z_vals = np.linspace(0, 0.4, num_steps, endpoint=False)
 
     x_direction_third_part_x_vals = np.linspace(1.2,  1.99,num_steps, endpoint=False) 
     #I end a bit away from the end, so that no two points are identitcal.
@@ -84,3 +84,103 @@ def interwoven_grid(num_steps=20):
     a = np.array([x_vals,y_vals,z_vals]).transpose()
     
     return a
+
+
+
+
+
+
+
+
+def interwoven_grid_222(num_steps=20):
+    """
+    interwoven 3D grid with index 2*2*2
+    """
+    
+    
+    x_direction_first_part_x_vals = np.linspace(0.01, 0.3, num_steps, endpoint = False) #I start a bit away from the end, so that no two points are identitcal.
+    x_direction_first_part_y_vals = np.linspace(0,  0.4, num_steps, endpoint = False)
+    x_direction_first_part_z_vals = np.linspace(0,  0, num_steps, endpoint = False)
+
+    x_direction_second_part_x_vals = np.linspace(0.3, 1.2, num_steps, endpoint = False) 
+    x_direction_second_part_y_vals = np.linspace(0.4, 0.2, num_steps, endpoint = False)
+    x_direction_second_part_z_vals = np.linspace(0, 0.4, num_steps, endpoint = False)
+
+    x_direction_third_part_x_vals = np.linspace(1.2,  1.99,num_steps, endpoint = False) #I end a bit away from the end, so that no two points are identitcal.
+    x_direction_third_part_y_vals = np.linspace(0.2,  0, num_steps, endpoint = False)
+    x_direction_third_part_z_vals = np.linspace(0.4,  0, num_steps, endpoint = False)
+
+
+    y_direction_first_part_x_vals = np.linspace(0,  0, num_steps, endpoint = False) 
+    y_direction_first_part_y_vals = np.linspace(0.01, 0.3, num_steps, endpoint = False) #I start a bit away from the end, so that no two points are identitcal.
+    y_direction_first_part_z_vals = np.linspace(0,  0.4, num_steps, endpoint = False)
+
+    y_direction_second_part_x_vals = np.linspace(0, 0.4, num_steps, endpoint = False) 
+    y_direction_second_part_y_vals = np.linspace(0.3, 1.2, num_steps, endpoint = False)
+    y_direction_second_part_z_vals = np.linspace(0.4, 0.2, num_steps, endpoint = False)
+
+    y_direction_third_part_x_vals = np.linspace(0.4,  0, num_steps, endpoint = False) 
+    y_direction_third_part_y_vals = np.linspace(1.2,  1.99,num_steps, endpoint = False) #I end a bit away from the end, so that no two points are identitcal.
+    y_direction_third_part_z_vals = np.linspace(0.2,  0, num_steps, endpoint = False)
+
+
+    z_direction_first_part_x_vals = np.linspace(0,  0.4, num_steps, endpoint = False) 
+    z_direction_first_part_y_vals = np.linspace(0,  0, num_steps, endpoint = False)
+    z_direction_first_part_z_vals = np.linspace(0.01, 0.3, num_steps, endpoint = False) #I start a bit away from the end, so that no two points are identitcal.
+
+    z_direction_second_part_x_vals = np.linspace(0.4, 0.2, num_steps, endpoint = False) 
+    z_direction_second_part_y_vals = np.linspace(0, 0.4, num_steps, endpoint = False)
+    z_direction_second_part_z_vals = np.linspace(0.3, 1.2, num_steps, endpoint = False)
+
+    z_direction_third_part_x_vals = np.linspace(0.2,  0, num_steps, endpoint = False) 
+    z_direction_third_part_y_vals = np.linspace(0.4,  0, num_steps, endpoint = False)
+    z_direction_third_part_z_vals = np.linspace(1.2,  1.99, num_steps, endpoint = False) #I end a bit away from the end, so that no two points are identitcal.
+
+
+
+    x_vals = np.concatenate((x_direction_first_part_x_vals, 
+                             x_direction_second_part_x_vals, 
+                             x_direction_third_part_x_vals, 
+                             y_direction_first_part_x_vals, 
+                             y_direction_second_part_x_vals, 
+                             y_direction_third_part_x_vals, 
+                             z_direction_first_part_x_vals, 
+                             z_direction_second_part_x_vals, 
+                             z_direction_third_part_x_vals))
+    
+    y_vals = np.concatenate((x_direction_first_part_y_vals, 
+                             x_direction_second_part_y_vals, 
+                             x_direction_third_part_y_vals, 
+                             y_direction_first_part_y_vals, 
+                             y_direction_second_part_y_vals, 
+                             y_direction_third_part_y_vals, 
+                             z_direction_first_part_y_vals, 
+                             z_direction_second_part_y_vals, 
+                             z_direction_third_part_y_vals))
+    
+    z_vals = np.concatenate((x_direction_first_part_z_vals, 
+                             x_direction_second_part_z_vals, 
+                             x_direction_third_part_z_vals, 
+                             y_direction_first_part_z_vals, 
+                             y_direction_second_part_z_vals, 
+                             y_direction_third_part_z_vals, 
+                             z_direction_first_part_z_vals, 
+                             z_direction_second_part_z_vals, 
+                             z_direction_third_part_z_vals))
+
+
+    #To bring it into one unit cell (the unit cell is the standard 1x1x1 cubic unit cell)
+    x_vals = x_vals - np.floor(x_vals)
+    y_vals = y_vals - np.floor(y_vals) 
+    z_vals = z_vals - np.floor(z_vals)
+
+     a = np.array([x_vals,y_vals,z_vals]).transpose()
+    
+    
+
+    return a
+
+
+
+
+
