7f610832f08dc37814c2627eaf0a9e2816493229
Chiara Martyka <chiara.martyka@gmail.com>
2023-02-03 11:25:10 +0100

    Create file for preprocessing of input data

--- periodic_ph/utils/.ipynb_checkpoints/simplex_classes-checkpoint.py
+++ periodic_ph/utils/.ipynb_checkpoints/simplex_classes-checkpoint.py
@@ -1,5 +1,37 @@
 # libraries
 import numpy as np
+from dataclasses import dataclass
+
+
+# CHANGE SIMPLEX CLASSES TO DATACLASSES
+"""
+
+@dataclass
+class Simplex:
+    verts: list
+    index_total: int
+    index_dim: int
+    index_cont: float
+    dim: int = len
+    
+    
+    name: str
+    unit_price: float
+    quantity_on_hand: int = 0
+
+    def total_cost(self) -> float:
+        return self.unit_price * self.quantity_on_hand
+
+# will add, among other things, a __init__() that looks like:
+
+def __init__(self, name: str, unit_price: float, quantity_on_hand: int = 0):
+    self.name = name
+    self.unit_price = unit_price
+    self.quantity_on_hand = quantity_on_hand
+
+
+
+"""
 
 
 
